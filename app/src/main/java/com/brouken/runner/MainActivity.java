85cea410c7b907bc3dd564e2c1bb07329cd22553
Marcel Dopita <m@rcel.cz>
2022-10-09 22:37:03 +0200

    Initial release

---
+++ app/src/main/java/com/brouken/runner/MainActivity.java
@@ -0,0 +1,29 @@
+package com.brouken.runner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        PackageManager packageManager = getPackageManager();
+        for (ApplicationInfo applicationInfo : packageManager.getInstalledApplications(0)) {
+            if((applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0){
+                if (!applicationInfo.enabled) {
+                    Intent launchIntent = packageManager.getLaunchIntentForPackage(applicationInfo.packageName);
+                    if (launchIntent != null) {
+                        startActivity(launchIntent);
+                    }
+                }
+            }
+        }
+
+        finish();
+    }
+}
